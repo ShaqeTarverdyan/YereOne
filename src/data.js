12394fdcabf35717263b3dca80d55fe222fd7d43
shaqe9301 <tshaqe@gmail.com>
2019-06-21 09:15:53 +0400

    add portfolio data

--- src/data.js
+++ src/data.js
@@ -35,16 +35,73 @@ export const servicesData = [
 
 export const recentWorks = [
     {
+        id:1,
+        name:'Website Developement for Colmag',
         image:'https://www.yereone.com/static/version1560548904/frontend/Yereone/default/en_US/Yereone_Portfolio/images/colmag-600x600.jpg',
+        image2:'https://www.yereone.com/media/portfolio/colmag-3-screens1-1200x1200.jpg',
+        image3:'https://www.yereone.com/media/portfolio/colmag-3-screens-1200x1200.jpg',
+        content1:'With 20 years of successful experience, Colmag offers the quality of reading glasses and sunglasses, Colmag Boutique is also available online. An incomparable choice of classic and trendy style in a wide range of colors at a discounted price. All forms and styles rounded or square, oversized or oval, aviator or clubmaster, there is something for all tastes.',
+        content2:"Our client wanted his website developement to be done on Magento 2 platform Our client provided us with the PSD files and the idea how they want to work his website after the end of this project.We have created some functionalities that help our client sell his product online."
     },
     {
+        id:2,
+        name:'Meatfish',
         image:'https://www.yereone.com/static/version1560548904/frontend/Yereone/default/en_US/Yereone_Portfolio/images/meatfish-400x400.png',
+        image2:'https://www.yereone.com/media/portfolio/meatfish-screens-600.png',
+        image3:'https://www.yereone.com/media/portfolio/meatfish-iphone.jpg',
+        contact1:'Meatfish is a dutch company selling meats of fishes. This is their online store, where you can buy and order meat.The current project was executed by our specialists in 2016 within 2 month.',
+        content2:'Meatfish.nl is based on Magento v1.9. Client provided PSD files and we followed them to make website look like how client wanted. We did a lot of customisations as per client’s requirements. We changed home page layout, banners, did the responsive design. Product, category, cart and checkout pages are also modified as per client’s PSD files.'
     },
     {
+        id:3,
+        name:'WeddingRings.co.uk Online Store',
         image:'https://www.yereone.com/static/version1560548904/frontend/Yereone/default/en_US/Yereone_Portfolio/images/wedding-rings-600x600.jpg',
+        image2:'https://www.yereone.com/media/portfolio/wedding-rings-co-uk-iphone-1024x1024.jpg',
+        image3:'https://www.yereone.com/media/portfolio/wedding-rings-co-uk-screens-600x600.png',
+        content1:'We are proud to present you our new partner, who has entrusted us with the creation of an online store. Wedding-Rings.co.uk is part of Hathaway Styles Limited, online retailers of high quality jewellery. They have been manufacturing quality jewellery in the UK for over 80 years which is sold throughout the UK in some of the finest jewellers. They are highly passionate about jewellery and have been involved in the jewellery industry for many years. Everyone can buy a handmade wedding ring that has been individually made by highly skilled craftspeople to their exacting requirements. They have access to talented designers and trades such as the assay office, all of which helps to make their business very efficient and enables them to send the chosen wedding ring without delay.',
+        content2:'Website is based on the Magento 2 platform. We implemented the provided PSD files and turned it into fully functional Magento 2 Online Shop, which has everything for doing online shopping.'
     },
     {
+        id:4,
+        name:'Classiccreations',
         image:'https://www.yereone.com/static/version1560548904/frontend/Yereone/default/en_US/Yereone_Portfolio/images/classiccreations-400x400.jpg',
+        image2:'https://www.yereone.com/media/portfolio/classic-creations-screens.png',
+        image3:'https://www.yereone.com/media/portfolio/classic-creations-mobile.jpg',
+        content1:'Classic Creations is a Canadian company, selling wedding and engagement rings. The webshop was created by Yereone in close cooperation with Classic Creations and using the Magento 1 eCommerce platform. The project was executed by our specialists in 2016 within 2 months.',
+        content2:'Classiccreations.com is based on Magento v1.9. It was creating according to the provided PSD files and instructions. We did a lot of customisations as per client’s requirements. We changed home page layout, banners, did the responsive design. Product, category, cart and checkout pages are also modified as per client’s PSD files.'
+    },
+    {
+        id:'5',
+        name:'Besselingbouwcombinatie',
+        image:'https://www.yereone.com/static/version1560548904/frontend/Yereone/default/en_US/Yereone_Portfolio/images/Besseling-300x300.jpg',
+        image2:'https://www.yereone.com/media/portfolio/besseling-screens.png',
+        image3:'https://www.yereone.com/media/portfolio/besseling-mobile.png',
+        content1:'Besselingbouwcombinatie is an Dutch company. The current project was executed by our specialists in 2016 within 1 month.',
+        content2:'Besselingbouwcombinatie.nl is based on WordPress CMS. Client provided PSD files and we followed them to make website look like how client wanted. We did a lot of customisations as per client’s requirements. We changed home page layout, banners, did the responsive design. Product, category, cart and checkout pages are also modified as per client’s PSD files.',
+        type:'website',
+        url:"besselingbouwcombinatie"
+    },
+    {
+        id:'6',
+        name:'Napoon',
+        image:'',
+        image2:'',
+        image3:'',
+        content1:'',
+        content2:'',
+        type:'',
+        url:""
+    },
+    {
+        id:'5',
+        name:'Besselingbouwcombinatie',
+        image:'',
+        image2:'',
+        image3:'',
+        content1:'',
+        content2:'',
+        type:'',
+        url:""
     }
 ]
 
